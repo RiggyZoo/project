a221128a7f7bc875ee62c272bd421885ae290d32
Nikita <example@gmail.com>
2020-07-24 23:18:19 +0200

    first task

--- weather.js
+++ weather.js
@@ -1,13 +1,21 @@
-"use strict";
-console.log ('arr'+ '- object');
 
-let incr = 10,
-    decr = 10;
+let numberOfFilm = +prompt('How many films did you watch?', '');
 
-incr++
-decr--
 
-const isChecked= true,
-      isClose=false;
+let personalMovieDB = {
+    count: numberOfFilm,
+    movies: {},
+    actors:{},
+    genres: [],
+    privat: false
+}
 
-console.log(isChecked&&isClose)
+const lastFilm = prompt('Один из послдених фильмов которые вы посмотрели?','');
+const scoreOfFilm = prompt('На сколько вы его оцените?', '');
+
+personalMovieDB.movies[lastFilm]= scoreOfFilm;
+
+console.log(personalMovieDB)
+
+console.log(personalMovieDB.count)
+
