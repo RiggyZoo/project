004d490e3b19fdc883c94b93101b421855cb4fe2
Nikita <example@gmail.com>
2020-07-24 22:26:21 +0200

    first commit

---
+++ weather.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log ('arr'+ '- object');
+
+let incr = 10,
+    decr = 10;
+
+incr++
+decr--
+
+const isChecked= true,
+      isClose=false;
+
+console.log(isChecked&&isClose)
