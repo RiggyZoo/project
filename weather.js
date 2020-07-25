2039d033a8e2f93d39bbd96d9501bc23f3336ba1
Nikita <example@gmail.com>
2020-07-25 14:22:18 +0200

    second task

--- weather.js
+++ weather.js
@@ -10,12 +10,34 @@ let personalMovieDB = {
     privat: false
 }
 
-const lastFilm = prompt('Один из послдених фильмов которые вы посмотрели?','');
-const scoreOfFilm = prompt('На сколько вы его оцените?', '');
+for (let i = 0; i<2;i++){
+    const lastFilm = prompt('Один из послдених фильмов которые вы посмотрели?','');
+    const scoreOfFilm = prompt('На сколько вы его оцените?', '');
+    if (lastFilm != null &&  scoreOfFilm != null && lastFilm !='' && scoreOfFilm != ''&& lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm]= scoreOfFilm;
+        console.log ('done')
+    } else {
+        console.log ('error');
+        i--;
+    }
+}
 
-personalMovieDB.movies[lastFilm]= scoreOfFilm;
+if (personalMovieDB.count < 10) {
+    console.log ('Просмотрено слишком мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count<30){
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >=30){
+    console.log('Вы - киноман');
+} else {
+    console.log('Произошла ошибка')
+}
 
 console.log(personalMovieDB)
 
 console.log(personalMovieDB.count)
 
+// if(personalMovieDB.count < 10) {
+//     document.write('Просмотрено слишком мало фильмов');
+// } else if (personalMovieDB.count > 10 && <30){
+//
+// }
